c00542b9fd540637c19f7e069773c8aa4f140c9f
Greg Kempe <greg@kempe.net>
2025-09-23 14:51:34 +0200

    fix multi-lingual urls

--- lawlibrary/settings.py
+++ lawlibrary/settings.py
@@ -62,6 +62,7 @@ SOCIALACCOUNT_PROVIDERS["openid_connect"] = {  # noqa
     ],
 }
 
+LANGUAGES = [("en", "English")]
 
 LOGGING["loggers"]["peachjam_pay"] = {"level": "DEBUG" if DEBUG else "INFO"}  # noqa
 
